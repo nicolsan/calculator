07d1da550505f0c6001140c3e31940985ea679b0
Nicolas Sanjaya <nicolas.sanjaya701@gmail.com>
2022-06-13 21:37:27 +0700

    finish calculator

---
+++ index.js
@@ -0,0 +1,34 @@
+//handle when the number is pressed. It renders the number into #input element
+function number(number) {
+    // TODO: answer here
+    let input = document.getElementById('input');
+    if (number == 'Del') {
+        input.value = input.value.slice(0, -1);
+    } else if (number == "AC") {
+        input.value = "";
+    } else {
+        input.value += number;
+    }
+}
+
+//handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
+function operator(operator) {
+    let input = document.getElementById('input');
+    if (operator == 'Del') {
+        input.value = input.value.slice(0, -1);
+    } else if (operator == "AC") {
+        // TODO: answer here
+        input.value = "";
+    } else {
+        // TODO: answer here
+        input.value += operator;
+    }
+}
+
+//handle when = button is pressed. It renders the result into #input element
+function calculate() {
+    // TODO: answer here
+    let input = document.getElementById('input');
+    let result = eval(input.value);
+    input.value = result;  
+}
